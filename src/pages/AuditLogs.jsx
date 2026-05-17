b67e68d36217100cd80e72fe63587bc387a1474f
Rosemary Nwoye <oziomarosemary3@gmail.com>
2026-05-17 22:44:03 +0000

    origin main

--- src/pages/AuditLogs.jsx
+++ src/pages/AuditLogs.jsx
@@ -4,14 +4,14 @@ import { ClipboardList } from "lucide-react";
 
 const logs = [
   { id: 1, color: "green", message: "Sunrise Clinic hospital admin added new radiology staff...", time: "Today, 10:42am" },
-  { id: 2, color: "green", message: "Afolabi Samuel added a new hospital.", time: "Today, 10:42am" },
-  { id: 3, color: "blue", message: "Login code generated for Riverland Clinic.", time: "Today, 10:42am" },
-  { id: 4, color: "red", message: "Multiple failed login attempts detected for Promise Land H...", time: "Today, 10:42am" },
-  { id: 5, color: "blue", message: "Daily sync completed for Bloom Private Clinic.", time: "Today, 10:42am" },
-  { id: 6, color: "red", message: "Hospital account temporarily suspended due to inactivity.", time: "Today, 10:42am" },
-  { id: 7, color: "green", message: "Diagnostic Centre connected to MedSync Hospital.", time: "Today, 10:42am" },
+  { id: 2, color: "green", message: "Afolabi Samuel added a new hospital.", time: "Today, 02:53pm" },
+  { id: 3, color: "blue", message: "Login code generated for Riverland Clinic.", time: "Yesterday, 10:42am" },
+  { id: 4, color: "red", message: "Multiple failed login attempts detected for Promise Land H...", time: "Monday, 10:48pm" },
+  { id: 5, color: "blue", message: "Daily sync completed for Bloom Private Clinic.", time: "Friday, 09:42am" },
+  { id: 6, color: "red", message: "Hospital account temporarily suspended due to inactivity.", time: "Tuesday, 10:42am" },
+  { id: 7, color: "green", message: "Diagnostic Centre connected to MedSync Hospital.", time: "Wednesday, 10:42pm" },
   { id: 8, color: "blue", message: "Login code generated for RiverLand Clinic.", time: "Today, 10:42am" },
-  { id: 9, color: "blue", message: "Login code generated for RiverLand Clinic.", time: "Today, 10:42am" },
+  { id: 9, color: "blue", message: "Login code generated for RiverLand Clinic.", time: "Friday, 10:50am" },
   { id: 10, color: "blue", message: "Login code generated for RiverLand Clinic.", time: "Today, 10:42am" },
 ];
 
